be68d9822844b7f46576a8a9c742e1a114af909a
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-09-21 19:05:49 +0700

    Clean-up App entrypoint

--- js/src/App.jsx
+++ js/src/App.jsx
@@ -6,6 +6,8 @@ import './App.css';
 function App() {
 	const [ count, setCount ] = useState( 0 );
 
+	const increment = () => setCount( count => count + 1 );
+
 	return (
 		<div className="App">
 			<div className="logos">
@@ -18,7 +20,7 @@ function App() {
 			</div>
 			<h2>Vite + React</h2>
 			<div className="card">
-				<button onClick={ () => setCount( count => count + 1 ) }>count is { count }</button>
+				<button onClick={ increment }>count is { count }</button>
 				<p>
 					Edit <code>src/App.jsx</code> and save to test HMR
 				</p>
