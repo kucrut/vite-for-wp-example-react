2a05d4dcfd9705260e1c976ce6b62b78f5b85515
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-09-21 19:04:30 +0700

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
+// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import { wp_globals } from '@kucrut/vite-for-wp/utils';
 import create_config from '@kucrut/vite-for-wp';
+import external_globals from 'rollup-plugin-external-globals';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default create_config( 'js/src/main.jsx', 'js/dist', {
-	plugins: [ react() ],
+export default defineConfig( ( { command } ) => {
+	if ( command === 'serve' ) {
+		return create_config( 'js/src/main.jsx', 'js/dist', {
+			plugins: [ react() ],
+		} );
+	}
+
+	return create_config( 'js/src/main.jsx', 'js/dist', {
+		plugins: [
+			react(),
+			external_globals( {
+				...wp_globals(),
+			} ),
+		],
+		build: {
+			rollupOptions: {
+				external: [ 'react', 'react-dom' ],
+				output: {
+					globals: wp_globals(),
+				},
+			},
+		},
+	} );
 } );
