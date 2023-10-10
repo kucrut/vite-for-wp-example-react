d89b984a838e0ab81ce6e1042eb59a2b65fe6fd8
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-10-10 17:42:42 +0700

    Update vite config to use v4wp() and wp_scripts() plugins

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,16 @@
-// https://vitejs.dev/config/
-import { defineConfig } from 'vite';
-import { wp_globals } from '@kucrut/vite-for-wp/utils';
-import create_config from '@kucrut/vite-for-wp';
-import external_globals from 'rollup-plugin-external-globals';
+import { v4wp } from '@kucrut/vite-for-wp';
+import { wp_scripts } from '@kucrut/vite-for-wp/plugins';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig( ( { command } ) => {
-	if ( command === 'serve' ) {
-		return create_config( 'js/src/main.jsx', 'js/dist', {
-			plugins: [ react() ],
-		} );
-	}
-
-	return create_config( 'js/src/main.jsx', 'js/dist', {
-		plugins: [
-			react(),
-			external_globals( wp_globals(), {
-				include: [ '**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx' ],
-			} ),
-		],
-		build: {
-			rollupOptions: {
-				external: [ 'react', 'react-dom' ],
-				output: {
-					globals: wp_globals(),
-				},
-			},
-		},
-	} );
-} );
+export default {
+	plugins: [
+		v4wp( {
+			input: 'js/src/main.jsx',
+			outDir: 'js/dist',
+		} ),
+		wp_scripts(),
+		react( {
+			jsxRuntime: 'classic',
+		} ),
+	],
+};
