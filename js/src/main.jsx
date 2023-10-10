e1d5058580996aa55fdd90d09a13bcb85db33e2e
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-10-10 17:14:34 +0700

    Main entrypoint: Guard against missing root element

--- js/src/main.jsx
+++ js/src/main.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.createRoot( document.getElementById( 'my-app' ) ).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-);
+const el = document.getElementById( 'my-app' );
+
+if ( el ) {
+	ReactDOM.createRoot( el ).render(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>,
+	);
+}
